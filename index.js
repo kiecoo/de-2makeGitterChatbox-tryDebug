3b6842d9bf8c67ad17161f226c3268f0f20ffe60
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-04-09 16:11:37 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = gitterChat
+
+function makeGitterURL (username) {
+  return `https://gitter.im/${username}/~embed`
+}
+
+var exampleProfile = {
+  name: 'nina',
+  username: 'ninabreznik',
+  photo:'https://nomadlist.com/assets/img/cities/phuket-thailand-500px.jpg'
+}
+
+
+	        //difference from the repo of makeGitterChatbox https://github.com/kiecoo/makeGitterChatbox/blob/master/index.html
+		var x = makeGitterChatbox(exampleProfile)
+		document.body.appendChild(x)
