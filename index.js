7fdc052d6242ee1bdad6eb890ef77544e84e7b1c
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-04-11 12:20:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ function makeGitterURL (username) {
   return `https://gitter.im/${username}/~embed`
 }
 
-var exampleProfile = {
-  name: 'nina',
-  username: 'ninabreznik',
-  photo:'https://nomadlist.com/assets/img/cities/phuket-thailand-500px.jpg'
-}
+
 
 
 	        //difference from the repo of makeGitterChatbox https://github.com/kiecoo/makeGitterChatbox/blob/master/index.html
